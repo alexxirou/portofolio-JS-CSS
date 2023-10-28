c02bd6c9060b356a35eee57044d4f3cefd136bd1
Alexandros Xirouchakis <alex.xirou@hotmail.com>
2023-10-28 22:23:18 +0300

    Update header.js replaced timeout functions with promise chain for showContent function

--- js/header.js
+++ js/header.js
@@ -40,50 +40,43 @@ root.addEventListener("touchmove", handleMove);
 
 
 function showContent() {
-  const preloader = document.querySelector('.preloader');
-  const content = document.querySelector('.content');
-  const h1Element = document.querySelector('.loading-title');
-  const mainPage = document.querySelector('.main-page');
-
-  // Add a class to hide the preloader after the transition ends
-  preloader.addEventListener('transitionend', function () {
-    preloader.style.display = 'none';
-  });
-
+  const preloader = document.querySelector(".preloader");
+  const content = document.querySelector(".content");
+  const h1Element = document.querySelector(".loading-title");
+  const mainPage = document.querySelector(".main-page");
+ 
   // Delay the visibility of the preloader for a certain duration (e.g., 2 seconds)
-  setTimeout(function () {
-    preloader.classList.add('visible');
-  }); // Adjust the delay to your desired time (in milliseconds)
-
-  // Add the "visible" class to show the content section
-  setTimeout(function () {
-    content.classList.remove('hidden');
-  }, 2500);
-
-  // Add an animation class to slide up the preloader
-  setTimeout(function () {
-    preloader.classList.add('slide-down');
-  }, 2500); // Keep the delay at 100 milliseconds for the animation
-
-  setTimeout(function () {
-    //Make the main-page visible
-    mainPage.style.display = 'block';
-  }, 4500);
-
+  delay(2000)
+    .then(() => {
+      preloader.classList.add("visible");
+      return delay(500); // Return a promise to initiate the next action after 500ms
+    })
+    .then(() => {
+      content.classList.remove("hidden");
+    })
+    .then(() => {
+      preloader.classList.add("slide-down")
+      return delay(2000);
+    })
+    .then(() => {
+      mainPage.style.display = "block";
+      return delay(1000);
+    })
+    .then(()=>{
+      preloader.style.display = "none";
+    }
+    )
+  
   let opacity = 1;
   const fadeOutInterval = setInterval(function () {
     opacity -= 0.05;
     h1Element.style.opacity = opacity;
     if (opacity <= 0) {
       clearInterval(fadeOutInterval);
-      h1Element.style.display = 'none';
+      h1Element.style.display = "none";
     }
   }, 150); // Adjust the interval time (in milliseconds) for the gradual fade-out effect
 
-  // Hide the preloader after a certain duration
-  setTimeout(function () {
-    preloader.style.display = 'none';
-  }, 5500); // Adjust the delay to your desired time (in milliseconds)
 }
 
 function animateTextMorph() {
